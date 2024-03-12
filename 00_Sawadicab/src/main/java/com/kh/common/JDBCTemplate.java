523396792567de663c597f2f4426e8447ae63ba9
taegeon <taegeon9358@naver.com>
2024-03-12 19:15:36 +0900

    24-3-12

---
+++ 00_Sawadicab/src/main/java/com/kh/common/JDBCTemplate.java
@@ -0,0 +1,100 @@
+package com.kh.common;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCTemplate {
+	public static Connection getConnection() {
+		Connection conn = null;
+		Properties prop = new Properties();
+		
+		String filepath = JDBCTemplate.class.getResource("db/driver/driver.properties").getFile();
+		
+		try {
+			prop.load(new FileInputStream(filepath));
+			Class.forName(prop.getProperty("driver"));
+			conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("userName"),prop.getProperty("password"));
+			
+			conn.setAutoCommit(false);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return conn;
+		
+	}
+	
+	public static void commit(Connection conn) {
+		
+		try {
+			if(conn != null && ! conn.isClosed()) {
+					conn.commit();
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	public static void rollback(Connection conn) {
+		
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(ResultSet rset) {
+		try {
+			if(rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Statement stat) {
+		
+		try {
+			if(stat != null && !stat.isClosed()) {
+				stat.close();
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+}
---
+++ 00_Sawadicab/src/main/java/com/kh/member/model/vo/Member.java
@@ -0,0 +1,65 @@
+package com.kh.member.model.vo;
+
+import java.sql.Date;
+
+public class Member {
+	
+	String memberId;
+	String memberPwd;
+	String memberName;
+	Date birthDate;
+	int Phone;
+	String adminCheck;
+	
+	
+	public Member(String memberId, String memberPwd, String memberName, Date birthDate, int phone, String adminCheck) {
+		super();
+		this.memberId = memberId;
+		this.memberPwd = memberPwd;
+		this.memberName = memberName;
+		this.birthDate = birthDate;
+		Phone = phone;
+		this.adminCheck = adminCheck;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPwd() {
+		return memberPwd;
+	}
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public int getPhone() {
+		return Phone;
+	}
+	public void setPhone(int phone) {
+		Phone = phone;
+	}
+	public String getAdminCheck() {
+		return adminCheck;
+	}
+	public void setAdminCheck(String adminCheck) {
+		this.adminCheck = adminCheck;
+	}
+	@Override
+	public String toString() {
+		return "Member [memberId=" + memberId + ", memberPwd=" + memberPwd + ", memberName=" + memberName
+				+ ", birthDate=" + birthDate + ", Phone=" + Phone + ", adminCheck=" + adminCheck + "]";
+	}
+}
