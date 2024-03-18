cedfb3d5ca092015ede06f141e2c42730686bcf8
taegeon <taegeon9358@naver.com>
2024-03-18 17:44:26 +0900

    5-44

--- 00_Sawadicab/src/main/webapp/resources/script/script.js
+++ 00_Sawadicab/src/main/webapp/resources/script/script.js
@@ -2,6 +2,7 @@ const carousel = document.querySelector(".carousel"),
 firstImg = carousel.querySelectorAll("img")[0],
 arrowIcons = document.querySelectorAll(".wrapper i");
 
+
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
 const showHideIcons = () => {
