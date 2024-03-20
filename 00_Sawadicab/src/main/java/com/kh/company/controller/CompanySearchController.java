c37c54d4c8cd2dcb0b05c0feabba87e7d6552a6d
taegeon <taegeon9358@naver.com>
2024-03-20 17:48:26 +0900

    companylistPage 만듬

--- 00_Sawadicab/src/main/java/com/kh/company/controller/CompanySearchController.java
+++ 00_Sawadicab/src/main/java/com/kh/company/controller/CompanySearchController.java
@@ -36,11 +36,9 @@ public class CompanySearchController extends HttpServlet {
 		Date date1 = Date.valueOf(request.getParameter("date1"));
 		Date date2 = Date.valueOf(request.getParameter("date2"));
 		
-		Company c = new Company(search, date1, date2);
-		
-		Company cp = new CompanyService().selectCompany(c);
 		
 		request.getSession().setAttribute("searchVal", searchVal);
+		
 	}
 
 	/**
